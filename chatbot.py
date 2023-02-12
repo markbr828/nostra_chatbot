38277482515213bceeb79fb590ef28cdf72a980d
LY <happydreamman@gmail.com>
2023-02-12 14:53:04 +0000

    update

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+def generate_prompt(question):
+    return """{}""".format(
+        question.strip()
+    )
+
+def answer(question):
+    response = openai.Completion.create(
+        model="curie:ft-blockx-2023-02-12-13-06-40",
+        prompt=generate_prompt(question),
+        max_tokens=40,
+        temperature=0.1,
+    )
+    # print("response: ", response.choices[0].text)
+    return response.choices[0].text.strip().split("\n")[0]
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "GET":
+        print(request)
+def main():
+    print("Hello, I am Nosta sales manager! Ask me anything about Nosta.\n----------------------------------------\n")
+    while True:
+        question = input("Me: ")
+        if question.strip():
+            print("Nosta: "+answer(question))
+        print("\n")
+main()
